60e2b361cff1cd95a9e6eed47ea511e7a4cf8c3e
Sai Charan Bandi <charan.sai131@gmail.com>
2022-08-13 21:08:28 +0200

    magit and other important additions

--- init.el
+++ init.el
@@ -40,4 +40,103 @@
       `((".*" ,temporary-file-directory t)))
 
 
-;; at some point load sane-defaults
+;; Write all autosave files in the tmp dir
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Don't write lock-files, I'm the only one here
+(setq create-lockfiles nil)
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
+
+;; Setup packages
+(require 'setup-package)
+
+;; Install extensions if they're missing
+(defun init--install-packages ()
+  (packages-install
+   '(
+;;     cider
+;;     clojure-mode
+;;     clojure-mode-extra-font-locking
+;;     css-eldoc
+     diff-hl
+;;     deadgrep
+;;     dockerfile-mode
+;;     edn
+;;     elisp-slime-nav
+;;     f
+;;     fill-column-indicator
+;;     flx
+;;     flx-ido
+     flycheck
+;;     flycheck-pos-tip
+;;     flycheck-joker
+     forge
+;;     gist
+;;     groovy-mode
+;;     guide-key
+;;     highlight-escape-sequences
+;;     htmlize
+;;     hydra
+;;     ido-at-point
+     ;; ido-completing-read+
+     ;; ido-vertical-mode
+     ;; inflections
+     ;; kaocha-runner
+     magit
+     ;; markdown-mode
+     ;; move-text
+     ;; nodejs-repl
+     ;; paredit
+     ;; perspective
+     ;; prodigy
+     ;; projectile
+     ;; request
+     ;; restclient
+     ;; ripgrep
+     ;; simple-httpd
+     smartparens
+     ;; string-edit
+     ;; visual-regexp
+     ;; wgrep
+     ;; whitespace-cleanup-mode
+     ;; yasnippet
+     ;; zprint-mode
+     )))
+
+(condition-case nil
+    (init--install-packages)
+  (error
+   (package-refresh-contents)
+   (init--install-packages)))
+
+;; load stuff
+(require 'sane-defaults)
+(require 'key-bindings)
+(require 'setup-magit)
+
+;; Functions (load all files in defuns-dir)
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+(dolist (file (directory-files defuns-dir t "\\w+"))
+  (when (file-regular-p file)
+    (load file)))
+
+;;flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Browse kill ring
+(require 'browse-kill-ring)
+(setq browse-kill-ring-quit-action 'save-and-restore)
+
+;; Smart M-x is smart
+(require 'smex)
+(smex-initialize)
--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -5,3 +5,39 @@
 
 ;; Eval buffer
 (global-set-key (kbd "C-c C-k") 'eval-buffer)
+
+;;Create new frame
+(define-key global-map (kbd "C-x C-n") 'make-frame-command)
+
+;; Comment/uncomment block
+(defun comment-or-uncomment-line-or-region ()
+  "Comments or uncomments the current line or region."
+  (interactive)
+  (if (region-active-p)
+      (comment-or-uncomment-region (region-beginning) (region-end))
+    (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    )
+  )
+(global-set-key (kbd "C-c c") 'comment-or-uncomment-line-or-region)
+;;(global-set-key (kbd "C-c u") 'uncomment-region)
+;;(global-set-key (kbd "C-c C") 'toggle-comment-on-line)
+
+;; Magit
+(global-set-key (kbd "C-x m") 'magit-status-fullscreen)
+(autoload 'magit-status-fullscreen "magit")
+
+;; Browse the kill ring
+(global-set-key (kbd "C-x C-y") 'browse-kill-ring)
+
+(global-set-key (kbd "C-x 3") 'split-window-right-and-move-there-dammit)
+
+;; Create scratch buffer
+(global-set-key (kbd "C-c b") 'create-scratch-buffer)
+
+;; Move windows, even in org-mode
+(global-set-key (kbd "<M-S-right>") 'windmove-right)
+(global-set-key (kbd "<M-S-left>") 'windmove-left)
+(global-set-key (kbd "<M-S-up>") 'windmove-up)
+(global-set-key (kbd "<M-S-down>") 'windmove-down)
+
+(provide 'key-bindings)
